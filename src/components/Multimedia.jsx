a534bf594849866ae5b878127577b7f045874287
Blessing Onwunali <blessing.onwunali@interswitchng.com>
2024-04-21 20:49:27 +0100

    feat: repush

--- src/components/Multimedia.jsx
+++ src/components/Multimedia.jsx
@@ -5,7 +5,7 @@ function App( {image , title, author}) {
   const [count, setCount] = useState(0)
 
   return (
-    <div className='flex flex-col items-center justify-center'>
+    <div className='flex flex-col items-center justify-center' >
         <img className='w-[87px] h-[87px] rounded-full ' src={image}/>
         <p className='text-white'>{title}</p>
         <p className='text-secondary text-sm'>{author}</p>
